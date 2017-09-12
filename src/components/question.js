4f9b083620c7c95791bde233171b707cd8c6d4cb
gabidavila <gabidavila@gmail.com>
2017-09-12 16:39:59 -0400

    adds navbar and question form

--- src/components/question.js
+++ src/components/question.js
@@ -11,12 +11,12 @@ class Question {
   render() {
     return `<div class="ui raised segment" data-questionid='${this.id}' data-props='${JSON.stringify(this)}' class='question-element'>
     <h3 class="ui dividing header">
-      ${this.title}
-      <span class="ui right floated"><i data-action='delete-question' class="trash icon"></i></span>
+      <a href="/questions/${this.id}" title="${this.title}">${this.title}</a>
+      <span class="floated-right"><i data-action='delete-question' class="trash icon"></i></span>
     </h3>
     <p>${this.content}</p>
     <div class="ui divider"></div>
-    <span>Asked by <a href="/users/${this.questioner.id}">${this.questioner.name}</a> on ${this.createdAt}</span>
+    <span>Asked by <a href="/users/${this.questioner.id}">${this.questioner.name}</a> on ${this.createdAt}</span> <span class="floated-right">${this.replies.length} replies</span>
     </div>`
   }
 }
