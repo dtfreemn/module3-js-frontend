7c87072802a6046f9f8f5800ecc2edfd5c0fb720
gabidavila <gabidavila@gmail.com>
2017-09-13 14:57:18 -0400

    Added score to the replies

--- src/components/reply.js
+++ src/components/reply.js
@@ -11,13 +11,18 @@ class Reply {
 
 	render() {
 		return `<div class="ui blue raised segment" data-replyid="${this.id}" data-questionid="${this.questionId}" data-props="${JSON.stringify(this)}" class="reply-element">
+
     <h3 class="ui dividing header">
       ${this.title}
       <span class="floated-right"><i data-questionid="${this.questionId}" data-replyid="${this.id}" data-action='delete-reply' class="trash icon"></i></span>
     </h3>
     <p>${this.content}</p>
     <div class="ui divider"></div>
-    <span>Posted by <a href="/users/${this.replier.id}">${this.replier.name}</a> on ${this.createdAt}</span> </span><span class="floated-right">${this.likes.length} likes</span> <span class="floated-right"><a href="#">Like</a>
+    <span>Posted by <a href="/users/${this.replier.id}">${this.replier.name}</a> on ${this.createdAt}</span>
+
+		<div class="ui bottom right attached label" style="margin-top: 20px; text-align:center">
+		<i class="arrow up icon" style="color:green"></i> <span class="likes" data-replyid="${this.id}">${this.likes.length} points &nbsp;</span>
+		<i class="arrow down icon" style="color: #CC0000"></i></div>
     </div>`
 	}
 }
